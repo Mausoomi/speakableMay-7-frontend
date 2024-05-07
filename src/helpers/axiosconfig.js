a8119fb696d5605f23c5d497b3970cd9a41b3ba1
unknown <dev23.mxpertz@gmail.com>
2024-05-07 15:47:44 +0530

    first commit

--- src/helpers/axiosconfig.js
+++ src/helpers/axiosconfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  // baseURL: "http://localhost:3000/api/",
   // baseURL: "/Backend_Speakable_English/api/",
-  // baseURL: "https://final-main-backend.onrender.com/api/",
+  baseURL: "https://speakablemay-7-backend.onrender.com/api/",
   withCredentials: true,
 });
 
